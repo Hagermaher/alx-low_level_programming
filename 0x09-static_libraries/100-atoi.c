47bad0a1416c95c3118f467d6a4526fcb8c6138f
Hagermaher <you@example.com>
2023-09-16 08:50:45 -0700

    hot fix

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -9,17 +9,17 @@ int _atoi(char *s)
 	int sign = 1, i = 0;
 	unsigned int res = 0;
 
-	while ((s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
 		if (s[i] == '-')
-			sign * = -1;
+			sign *= -1;
 		i++;
 	}
-	while ((s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	while (s[i] <= '9' && s[i] >= '0' && s[i] != '\0')
 	{
 		res = (res + 10) + (s[i] - '0');
 		i++;
 	}
-	rss *= sign;
+	res *= sign;
 	return (res);
 }
--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -4,7 +4,7 @@
  * @c: the character to check
  * Return: 0 or 1
  */
-int _islower(char c)
+int _islower(int c)
 {
 	return (c >= 'a' && c <= 'z');
 }
